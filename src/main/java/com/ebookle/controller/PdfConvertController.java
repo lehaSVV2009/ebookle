f189c444bdb7330a4551d2ab552c5ea7f42a412e
lehaSVV2009 <lehaSVV2009@gmail.com>
2013-08-31 17:16:29 +0300

    converter to pdf

--- src/main/java/com/ebookle/controller/PdfConvertController.java
+++ src/main/java/com/ebookle/controller/PdfConvertController.java
@@ -7,7 +7,10 @@ import com.ebookle.service.BookService;
 import com.ebookle.service.ChapterService;
 import com.ebookle.service.UserService;
 import com.ebookle.util.UtilStrings;
+import com.pdfcrowd.Client;
+import com.pdfcrowd.PdfcrowdError;
 import com.petebevin.markdown.MarkdownProcessor;
+import org.apache.commons.io.IOUtils;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -22,6 +25,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.util.List;
 
 /**
@@ -48,16 +53,40 @@ public class PdfConvertController {
 
     @Secured("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
     @RequestMapping("/{userLogin}/editBook/{bookTitle}/{chapterNumber}/savePdf")
-    public String savePdf (@PathVariable("userLogin") String userLogin,
+    public void savePdf (@PathVariable("userLogin") String userLogin,
                            @PathVariable("bookTitle") String bookTitle,
                            @PathVariable("chapterNumber") Integer chapterNumber,
-                           ModelMap modelMap) {
+                           HttpServletResponse response) {
 
-        User author = userService.findByLogin(userLogin);
-        Book book = bookService.findByTitleAndUserId(bookTitle, author);
-        List<Chapter> chapters = chapterService.findAllByBook(book);
-        String htmlBook = convertToHtmlBook(bookTitle, author.getName(), chapters);
-        return "pdf_reference";
+        try {
+
+            Client client = new Client("lehaSVV2009", "2c972a5566d152c96560e691e91b490c");
+            User author = userService.findByLogin(userLogin);
+            Book book = bookService.findByTitleAndUserId(bookTitle, author);
+            List<Chapter> chapters = chapterService.findAllByBook(book);
+            String htmlBook = convertToHtmlBook(bookTitle, author.getName(), chapters);
+            writeBookToPdf(client, htmlBook);
+            FileInputStream inputStream = new FileInputStream("d://ACTIVITY/serverResources/ebookle.pdf");
+            IOUtils.copy(inputStream, response.getOutputStream());
+            inputStream.close();
+            response.flushBuffer();
+        } catch  (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch(PdfcrowdError why) {
+            System.err.println(why.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        response.setContentType("application/pdf");
+        response.setHeader("Content-Disposition", "attachment; filename=ebookle.pdf");
+    }
+
+    private void writeBookToPdf (Client client, String htmlBook) throws IOException {
+        ByteArrayOutputStream memStream  = new ByteArrayOutputStream();
+        client.convertHtml(htmlBook, memStream);
+        FileOutputStream outputStream = new FileOutputStream("d://ACTIVITY/serverResources/ebookle.pdf");
+        outputStream.write(memStream.toByteArray());
+        outputStream.close();
     }
 
     private String convertToHtmlBook (String bookTitle, String authorName, List<Chapter> chapters) {
